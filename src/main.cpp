b9b98e138a3b3191117b51da4b30c80987ea11e8
Alexis Oblet <alexis.oblet@gmail.com>
2016-03-01 11:01:19 +0100

    Update opencv cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include "opencv2/opencv.hpp"
+#include "opencv2/aruco.hpp"
 using namespace cv;
 
 int main(int argc, char** argv) {
-    VideoCapture cap(1);
+    VideoCapture cap(-1);
 
     // open the default camera, use something different from 0 otherwise;
     // Check VideoCapture documentation.
-    if(!cap.open(1))
+    if(!cap.open(-1))
         return 0;
 
     while (1) {
